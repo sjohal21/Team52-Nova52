d4c4155e248ed62933828943d24a3ff9f66f5c86
Saihaj Johal <personal@sjohal.net>
2025-11-11 11:47:02 +0000

    Added placeholder home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="lofi">
+<head>
+	<meta charset="UTF-8">
+	<title>Nova52 Store</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+	<h1>Nova52 Store</h1>
+	<p>Website under construction.</p>
+</body>
