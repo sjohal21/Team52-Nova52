996b9f9bdb4030a97ad1540cc81e293bfdbdf3c6
Saihaj <personal@sjohal.net>
2025-11-23 17:52:20 +0000

    Added basket columns

--- database/migrations/2025_11_23_172518_create_baskets_table.php
+++ database/migrations/2025_11_23_172518_create_baskets_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('baskets', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
