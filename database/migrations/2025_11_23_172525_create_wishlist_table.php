e02ff3843cb510256710a8b5b49fc87adbecb749
Saihaj <personal@sjohal.net>
2025-11-23 17:57:52 +0000

    Added wishlist table fully

--- database/migrations/2025_11_23_172525_create_wishlist_table.php
+++ database/migrations/2025_11_23_172525_create_wishlist_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('wishlist', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->integer('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
             $table->timestamps();
         });
     }
