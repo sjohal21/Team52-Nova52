af62f323ede912e08efddb582eeadbb6681697cf
Saihaj Johal <personal@sjohal.net>
2025-11-24 15:50:30 +0000

    Added footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,18 +16,40 @@
             <a class="btn btn-ghost text-xl" href="/">Nova-52</a>
         </div>
         <div class="navbar-center">
-            <button class="btn btn-ghost">Home</button>
-            <button class="btn btn-ghost">Products</button>
-            <button class="btn btn-ghost">About Us</button>
-            <button class="btn btn-ghost">Contact</button>
-            <button class="btn btn-ghost">Wish List</button>
+            <a class="btn btn-ghost">Home</a>
+            <a class="btn btn-ghost">Products</a>
+            <a class="btn btn-ghost">About Us</a>
+            <a class="btn btn-ghost">Contact</a>
+            <a class="btn btn-ghost">Wish List</a>
         </div>
         <div class="navbar-end">
-            <button class="btn btn-ghost"><img src="{{ URL::to('/') }}/cart.svg" alt="Picture of a shopping cart" class="size-10"></button>
-            <button class="btn btn-ghost"><img src="{{ URL::to('/') }}/profile-picture.svg" alt="Picture for account" class="size-10"></button>
+            <a class="btn btn-ghost"><img src="{{ URL::to('/') }}/cart.svg" alt="Picture of a shopping cart" class="size-10"></a>
+            <a class="btn btn-ghost"><img src="{{ URL::to('/') }}/profile-picture.svg" alt="Picture for account" class="size-10"></a>
         </div>
     </nav>
     <main class="flex-1 container mx-auto px-4 py-8">
         {{$slot}}
     </main>
+    <footer class="footer sm:footer-horizontal bg-black text-white p-10">
+        <aside>
+            <h1 class="text-3xl font-bold">Nova52</h1>
+            <p class="text-2xl font-light">(tagline)</p>
+        </aside>
+        <nav>
+            <h4 class="font-bold">Quick links</h4>
+            <a class="link link-hover">Home</a>
+            <a class="link link-hover">Products</a>
+            <a class="link link-hover">About us</a>
+            <a class="link link-hover">Contact</a>
+            <a class="link link-hover">Wish list</a>
+            <a class="link link-hover">Admin log in</a>
+        </nav>
+        <nav>
+            <h4 class="font-bold">Support</h4>
+            <a class="link link-hover">FAQ</a>
+            <a class="link link-hover">Shipping & returns</a>
+            <a class="link link-hover">Privacy policy</a>
+            <a class="link link-hover">Terms & Conditions</a>
+        </nav>
+    </footer>
 </body>
