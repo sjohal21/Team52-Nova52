a6e1941011c0aa4d303f984a04bddbad70b86337
Saihaj Johal <personal@sjohal.net>
2025-11-24 11:43:36 +0000

    Added OrderItems migration

--- database/migrations/2025_11_23_172702_create_orderitems_table.php
+++ database/migrations/2025_11_23_172702_create_orderitems_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('orderitems', function (Blueprint $table) {
             $table->id();
+            $table->integer('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->integer('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
