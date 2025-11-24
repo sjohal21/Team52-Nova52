262d5bb552c88ed55543798a6857402ace737ec7
Saihaj <personal@sjohal.net>
2025-11-24 18:52:33 +0000

    Use integrated daisyUI rather than importing from web

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,10 +3,6 @@
     <meta charset="UTF-8">
     <!-- Set title based on title variable set by pages using layout-->
     <title>{{isset($title) ? $title . ' - Nova52' : 'Nova52'}}</title>
-    <!-- Set up daisyUI for CSS components building on Laravel's Tailwind support -->
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
