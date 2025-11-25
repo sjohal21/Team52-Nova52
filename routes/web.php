07f7406cd7c83bfa336b6c8bd1c85b2a20f5fadf
raman <raman@RamandeepLaptop.localdomain>
2025-11-25 17:29:49 +0000

    Added Basket Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;;
+use App\Http\Controllers\BasketController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\WishlistController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('home');
