27a02c8476a18c5cc6e48b25ec75b2ebc2201f79
Kieron <kcmpsci@gmail.com>
2025-11-25 15:52:31 +0000

    updated RegisterController as well as register routes and added LoginController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function create() {
+    public function show() {
         return view('auth.register');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,9 @@ Route::get('/', function () {
 });
 
 //Sign Up Routes
-Route::get('/register', [RegisterController::class,'show'])->name('register');
-Route::get('/register', [RegisterController::class,'register'])->name('register.submit');
+Route::get('/register', [RegisterController::class,'show'])
+->middleware('guest'); //makes sure only unauthenticated users go to this page
+->name('register');
+Route::get('/register', [RegisterController::class,'register'])
+->middleware('guest');
+->name('register.submit');
