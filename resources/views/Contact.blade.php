7bdf2bd0fef143cbd8b465d74a6f3c33771495b7
Saihaj Johal <personal@sjohal.net>
2025-11-28 10:42:02 +0000

    Nearly completed contact page

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -2,13 +2,53 @@
     <x-slot:title>
         Contact
     </x-slot:title>
-    <div class="flex flex-col bg-base-100">
+    <div class="flex flex-col bg-base-300">
         <div class="text-center p-5">
             <h2 class="text-3xl font-bold">Contact Us</h2>
             <h3 class="text-2xl p-2">Have a question? Get in touch with the Nova52 team.</h3>
         </div>
-        <div class="bg-base-200 p-5 text-center">
+        <div class="bg-base-100 p-5 text-center">
             <h2 class="text-3xl font-bold">Get in touch</h2>
+            <div class="grid grid-cols-2 gap-4 p-5">
+                <div class="card w-auto bg-base-300 p-3">
+                    <div class="card-body">
+                            <h2 class="text-2xl font-bold card-title">Address</h2>
+                            <div class="text-xl text-left">
+                                <p>Aston University</p>
+                                <p>Birmingham</p>
+                                <p>B4 7ET</p>
+                                <p>United Kingdom</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="card w-auto bg-base-300 p-3">
+                    <div class="card-body">
+                        <h2 class="text-2xl font-bold card-title">Email</h2>
+                        <a href="mailto:nova52@example.com" class="text-xl underline text-left">nova52@example.com</a>
+                    </div>
+                </div>
+                <div class="card w-auto bg-base-300 p-3">
+                    <div class="card-body">
+                        <h2 class="text-2xl font-bold card-title">Phone</h2>
+                        <a class="text-xl text-left" href="tel:01213335689">0121 333 5689</a>
+                    </div>
+                </div>
+                <div class="card w-auto bg-base-300 p-3">
+                    <div class="card-body">
+                        <h2 class="text-2xl font-bold card-title">Operating hours</h2>
+                        <div class="text-xl text-left">
+                            <p>Monday - Friday:</p>
+                            <p>9:00 - 17:00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-300 w-auto p-3">
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title font-bold text-2xl">Send us a message</h2>
+                    <p class="text-xl">Have a question or need support? Fill out the form below and we will get back to you as soon as possible.</p>
+                </div>
+            </div>
         </div>
     </div>
 </x-layout>
