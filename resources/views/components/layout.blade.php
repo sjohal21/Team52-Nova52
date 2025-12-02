72650d1df14a7573d0e6e05483e8fd8539f689a4
Saihaj Johal <personal@sjohal.net>
2025-12-02 12:21:16 +0000

    Moved existing icons to folder

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,8 +19,8 @@
             <a class="btn btn-ghost">Wish List</a>
         </div>
         <div class="navbar-end">
-            <a class="btn btn-ghost" href="{{URL::to('/basket')}}"><img src="{{ URL::to('/') }}/cart.svg" alt="Picture of a shopping cart" class="size-10"></a>
-            <a class="btn btn-ghost"><img src="{{ URL::to('/') }}/profile-picture.svg" alt="Picture for account" class="size-10"></a>
+            <a class="btn btn-ghost" href="{{URL::to('/basket')}}"><img src="{{ URL::to('/') }}/icons/cart.svg" alt="Picture of a shopping cart" class="size-10"></a>
+            <a class="btn btn-ghost"><img src="{{ URL::to('/') }}/icons/profile-picture.svg" alt="Picture for account" class="size-10"></a>
         </div>
     </nav>
     <main class="">
