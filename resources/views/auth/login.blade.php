f95ea74b93625f896e0a5bfc4407255605465420
Saihaj Johal <personal@sjohal.net>
2025-12-03 12:45:51 +0000

    Added basic login frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,5 +3,13 @@
         Log In
     </x-slot:title>
     <div>
+        <div class="flex flex-col bg-base-200">
+            <div>
+                <form method="post">
+                    <input type="email" placeholder="Email address" class="input" name="email">
+                    <input type="password" placeholder="Password" class="input" name="password">
+                    <input type="submit" class="btn">
+                </form>
+            </div>
     </div>
 </x-layout>
