98f14a6700a236cc35debebee3ed46c4010ebb41
Kieron <kcmpsci@gmail.com>
2025-12-03 13:19:26 +0000

    added all semi colons

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,9 +8,9 @@ class ProductController extends Controller
 {
     public function index() {
     //Gets all of the products from the database
-        $products = Products::all()
+        $products = Products::all();
 
-        return view('products.index',compact('products'))
+        return view('products.index',compact('products'));
     }
 
     public function search() {
@@ -18,23 +18,23 @@ class ProductController extends Controller
         //Make sure that the search isnt empty
        $request->validate([
         'search' => 'required|string|max:255'
-       ])
+       ]);
 
         //Retrieve the entered item for the search
-        $searchedItem = request->input('search')
+        $searchedItem = request->input('search');
 
         //Search for similar product to the product that is entered
-        $products = Product::where('name','like',"$%{$searchedItem}%")->get()
+        $products = Product::where('name','like',"$%{$searchedItem}%")->get();
         
         //if there are no products that match show a message
         if($products->isEmpty()) {
-            return->redirect()->back()->with()->('message','No products that match your seatch found')
+            return->redirect()->back()->with()->('message','No products that match your search found!!');
         }
 
-        return view('products.index',compact('products'))
+        return view('products.index',compact('products'));
     }
 
     public function showOne(Product $product) {
-        return view('products.show',compact('product'))
+        return view('products.show',compact('product'));
     }
 }
