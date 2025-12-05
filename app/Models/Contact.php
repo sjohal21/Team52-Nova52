ebb582266de295bae9e62e9056c64b16ca5d2ae6
Puzzle-Head94 <benjamin14brown@gmail.com>
2025-12-05 02:48:10 +0000

    Added contact us table

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     //
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'subject',
+        'description',
+    ];
+    
 }
