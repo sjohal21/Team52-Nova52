1a18eb81b9a2e43bc20f0c3f24d8567b2206c711
saff <official.saffronc@icloud.com>
2025-12-05 03:01:40 +0000

    Switched code for blade views into correct folders after recloning repository to bypass rebase errors

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,98 +1,116 @@
 <x-layout>
     <x-slot:title>
-        Products
+        {{$product->name}}
 </x-slot:title>
-<!-- the beginning part of the webpage/ content for the heading-->
- <section class="py-12 px-6 text-center">
-    <!-- this is for the box-->
-     <h1 class="text-3x1 md:text-4x1 font-bold mb-2">
-        <!-- this is the specs for the headings-->
-         Our Products
+<!-- this shows the names for the products-->
+ <section class="bg-base-200 py-10 text center">
+    <h1 class="text-3x1 md:text-4x1 font-bold">
+        {{$product->name}}
 </h1>
-<p class="text-sm md:text-base text-neutral">
-    Explore the latest tech across all of our categories
-</p>
-
-<!-- this is the specs for the search bar-->
- <div class="max-w-md mx-auto mt-6">
-    <form action="{{route('product.search')}}"method="GET"
-    class="flex items-center bg-base-100 rounded-full shadow px-4 py-3 gap-2">
-    <span class="text-lg"><!--search icon for the placeholder--></span>
-        <input
-        type="text"
-        name="q"
-        value="{{request('q') }}"
-        placeholder="search products..."
-        class="w-full bg-transparent outline-none text-sm"
-        >
-</form>
-</div>
-</section>
-<!-- this is for the content/ products within the catalog-->
- <section class="text-center mb-10">
-    <div class="flex flex-wrap justify-center gap-4">
-
-    <!--this section of code is so the users interact within the page-->
-    <a href="{{route('products.index')}}"
-    class="btn btn-sm rounded-full px-6{{request('category')?'btn-neutral'}}>
-    All
-</a>
-
-@foreach ($categories as $category)
-<a href="{{route('products.index,['category'=> $category->slug])}}">
-    <!-- these are all the icons for the products/ sections-->
-     @if ($category->name ==='Smartphones') [icon] @endif
-     @if ($category->name ==='laptops') [icon] @endif
-     @if ($category->name ==='Desktops') [icon] @endif
-     @if ($category->name ==='Audio') [icon] @endif
-     @if ($category->name ==='Tablets') [icon] @endif
-     {{$category->name}}
-</a>
-@endforeach
-
-</div>
 </section>
 
-<!-- this is for the product cards/grid-->
- <section class="max-w-7x1 mx-auto mb-20 px-4">
-    <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-        @forelse ($products as $product)
-        <div class="card bg-base-100 shadow-md rounded-2x1 pb-4 relative hover:shadow-x1 transition">
-            <!--design for the product-->
+<!--this is for the box-->
+<section class="max-w-6x1 mx-auto bg-base-200 rounded-2x1 p-10 mt-10">
+    <div class="grid md:grid-cols-2 gap-10">
 
-            <!--this is for adding the product to the wishlist-->
-            <button class="absolute right-3 top-3 text-lg opacity-80 hover:opacity-100">
-                [icon]
+    <!--this is for the left side of the page. product images will go here-->
+    <div>
+        <div class="relative rounded-x1 overflown-hidden shadow">
+
+        <!--this is for the heart-->
+        <buttton class="absolute top-3 right-3 text-xl opacity-80 hover:opacity-100">
+            [icon]
 </button>
 
-<!--this is for a image-->
-<figure class="h-40 overflown-hidden rounded-t-2x1">
-    <img src="{{$product->photo_url}}"
-    alt="{{$product->name}}"
-    class="W-full h-full object-cover">
-</figure>
+<img src="{{$product->photo_url}}"
+alt="{{$product->name}}"
+class="w-full h-80 object-cover rounded-x1">
+</div>
 
-<!--this is for the information within the card-->
-<div class="card-body text-center px-4">
-    <h3 class="font-semibod text-sm">{{$product->name}}</h3>
+<!--this is for the product image (underneath)-->
+<div class="flex gap-3 mt-4">
+    <img src="{{$product->photo_url}}" class="w-20 h-16 object-cover rounded-lg shadow">
+    <img src="{{$product->photo_url}}" class="w-20 h-16 object-cover rounded-lg shadow">
+    <img src="{{$product->photo_url}}" class="w-20 h-16 object-cover rounded-lg shadow">
+</div>
+</div>
 
-    <p class="text-xs text-neutral mb-3">
-        from £{{number_format($product->price,2)}}
+<!--this is for thr right section(info)-->
+<div class="space-y-6">
+
+<!--this is for the names for the product-->
+<h2 class="text-2x1 font-bold">{{$product->name}}</h2>
+
+<!--this is for if the product has a tagline-->
+@if(isset($product->taglie))
+<p class="text-sm text-neutral">{{$product->tagline}}
+</p>
+@endif
+
+<!--this is for the price-->
+<p class="text-lg font-bold">
+    price: £{{number_format($product->price,2)}}
 </p>
 
-<a href="{{route('products.show', $product->id) }}"
-class="btn btn-neutral btn-xs rounded-full px-4">
-view product
-</a>
-</div>
-</div>
-@empty
-<p class="text-center col-span-full text-sm md:text-base">
-    No peoducts found.
+<!--this is for how much stock is left for each product-->
+<p class="text-sm">
+    @if ($product->stock_level>10)
+    <span class="text-green-600 font-medium">In Stock:{{$product->stock_level}}</span>
+    @else
+    <span class="text-red-600 font-medium">Out of Stock</span>
+    @endif
 </p>
-@endforelse
 
+<!--this is for the specs of each product-->
+<ul class="text-sm list-none space-y-1">
+    <li>[icon] Feature 1</li>
+    <li>[icon] Feature 2</li>
+    <li>[icon] Feature 3</li>
+</ul>
+
+<!--this is for letting the customer know how much product they have selected-->
+<div class="flex items-center gap-3">
+    <span class="font-semibold">Quantity:</span>
+    <select class="select select-bordered select-sm w-20 rounded">
+        @for ($i=1; $i <=10; $i++)
+        <option>{[$i]}</option>
+        @endfor
+</select>
 </div>
+
+<!--this is is for the basket (button)-->
+<button class="btn btn-neutral w-full md:w-64 rounded-full text-white mt-42>
+Add To Basket
+</button>
+</div>
+</div>
+
+<!--this is for the information on each product-->
+@if($product->desription)
+<div class="border-t border-neutral/50 mt-14 pt-10">
+    <h2 class="text-center text-xl font-bold mb-4">
+        Product Description
+</h2>
+<p class="text-sm md:text-base leading-relaxed">
+    {{$product->description}}
+</p>
+</div>
+@endif
+
+<!--this is for product information-->
+@if(isset($product->specification))
+<div class="border-t border-neutral/50 mt-14 pt-10">
+    <h2 class="text-center text-xl font-bold mb-4">
+        Product specification
+</h2>
+<p class="text-center text-xl font-bold mb-4">
+    Product specification
+</h2>
+<p class="text-sm md:text-base leading-relaxed">
+    {{$product->specification}}
+</p>
+</div>
+@endif
 </section>
 </x-layout>
 
