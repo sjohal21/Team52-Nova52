c7b5ba8a53c240b206045fd303158c27b61d7427
Kieron <kcmpsci@gmail.com>
2025-12-05 11:56:33 +0000

    Removed currently unneeded features and fixed typos

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,28 +3,28 @@
         {{$product->name}}
 </x-slot:title>
 <!-- this shows the names for the products-->
- <section class="bg-base-200 py-10 text center">
-    <h1 class="text-3x1 md:text-4x1 font-bold">
+ <section class="bg-base-200 py-10 text-center">
+    <h1 class="text-3xl md:text-4xl font-bold">
         {{$product->name}}
 </h1>
 </section>
 
 <!--this is for the box-->
-<section class="max-w-6x1 mx-auto bg-base-200 rounded-2x1 p-10 mt-10">
+<section class="max-w-6xl mx-auto bg-base-200 rounded-2xl p-10 mt-10">
     <div class="grid md:grid-cols-2 gap-10">
 
     <!--this is for the left side of the page. product images will go here-->
     <div>
-        <div class="relative rounded-x1 overflown-hidden shadow">
+        <div class="relative rounded-xl overflow-hidden shadow">
 
         <!--this is for the heart-->
-        <buttton class="absolute top-3 right-3 text-xl opacity-80 hover:opacity-100">
+        <button class="absolute top-3 right-3 text-xl opacity-80 hover:opacity-100">
             [icon]
 </button>
 
 <img src="{{$product->photo_url}}"
 alt="{{$product->name}}"
-class="w-full h-80 object-cover rounded-x1">
+class="w-full h-80 object-cover rounded-xl">
 </div>
 
 <!--this is for the product image (underneath)-->
@@ -39,13 +39,7 @@ class="w-full h-80 object-cover rounded-x1">
 <div class="space-y-6">
 
 <!--this is for the names for the product-->
-<h2 class="text-2x1 font-bold">{{$product->name}}</h2>
-
-<!--this is for if the product has a tagline-->
-@if(isset($product->taglie))
-<p class="text-sm text-neutral">{{$product->tagline}}
-</p>
-@endif
+<h2 class="text-2xl font-bold">{{$product->name}}</h2>
 
 <!--this is for the price-->
 <p class="text-lg font-bold">
@@ -54,7 +48,7 @@ class="w-full h-80 object-cover rounded-x1">
 
 <!--this is for how much stock is left for each product-->
 <p class="text-sm">
-    @if ($product->stock_level>10)
+    @if ($product->stock_level !== null && $product->stock_level > 0)
     <span class="text-green-600 font-medium">In Stock:{{$product->stock_level}}</span>
     @else
     <span class="text-red-600 font-medium">Out of Stock</span>
@@ -73,41 +67,26 @@ class="w-full h-80 object-cover rounded-x1">
     <span class="font-semibold">Quantity:</span>
     <select class="select select-bordered select-sm w-20 rounded">
         @for ($i=1; $i <=10; $i++)
-        <option>{[$i]}</option>
+        <option>{{$i}}</option>
         @endfor
 </select>
 </div>
 
 <!--this is is for the basket (button)-->
-<button class="btn btn-neutral w-full md:w-64 rounded-full text-white mt-42>
+<button class="btn btn-neutral w-full md:w-64 rounded-full text-white mt-12">
 Add To Basket
 </button>
 </div>
 </div>
 
 <!--this is for the information on each product-->
-@if($product->desription)
+@if($product->description)
 <div class="border-t border-neutral/50 mt-14 pt-10">
     <h2 class="text-center text-xl font-bold mb-4">
         Product Description
 </h2>
 <p class="text-sm md:text-base leading-relaxed">
     {{$product->description}}
-</p>
-</div>
-@endif
-
-<!--this is for product information-->
-@if(isset($product->specification))
-<div class="border-t border-neutral/50 mt-14 pt-10">
-    <h2 class="text-center text-xl font-bold mb-4">
-        Product specification
-</h2>
-<p class="text-center text-xl font-bold mb-4">
-    Product specification
-</h2>
-<p class="text-sm md:text-base leading-relaxed">
-    {{$product->specification}}
 </p>
 </div>
 @endif
