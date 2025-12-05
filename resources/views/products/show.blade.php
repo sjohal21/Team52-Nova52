5170ab43268c1fa7cc42b20cd1ecc528beec1d1a
Saihaj Johal <personal@sjohal.net>
2025-12-05 12:24:37 +0000

    Revert "Merge fixes"
    This reverts commit 573d512cef3ac3c01201870ed54db2514dad3eca, reversing
    changes made to 0e3666b3a94d1632a7e5d003aa9bc50e794a936f.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,30 +1,30 @@
 <x-layout>
     <x-slot:title>
         {{$product->name}}
-</x-slot:title>
+    </x-slot:title>
 <!-- this shows the names for the products-->
- <section class="bg-base-200 py-10 text-center">
-    <h1 class="text-3xl md:text-4xl font-bold">
+ <section class="bg-base-200 py-10 text center">
+    <h1 class="text-3x1 md:text-4x1 font-bold">
         {{$product->name}}
 </h1>
 </section>
 
 <!--this is for the box-->
-<section class="max-w-6xl mx-auto bg-base-200 rounded-2xl p-10 mt-10">
+<section class="max-w-6x1 mx-auto bg-base-200 rounded-2x1 p-10 mt-10">
     <div class="grid md:grid-cols-2 gap-10">
 
     <!--this is for the left side of the page. product images will go here-->
     <div>
-        <div class="relative rounded-xl overflow-hidden shadow">
+        <div class="relative rounded-x1 overflown-hidden shadow">
 
         <!--this is for the heart-->
         <button class="absolute top-3 right-3 text-xl opacity-80 hover:opacity-100">
             [icon]
-</button>
+        </button>
 
 <img src="{{$product->photo_url}}"
 alt="{{$product->name}}"
-class="w-full h-80 object-cover rounded-xl">
+class="w-full h-80 object-cover rounded-x1">
 </div>
 
 <!--this is for the product image (underneath)-->
@@ -39,7 +39,13 @@ class="w-full h-80 object-cover rounded-xl">
 <div class="space-y-6">
 
 <!--this is for the names for the product-->
-<h2 class="text-2xl font-bold">{{$product->name}}</h2>
+<h2 class="text-2x1 font-bold">{{$product->name}}</h2>
+
+<!--this is for if the product has a tagline-->
+@if(isset($product->taglie))
+<p class="text-sm text-neutral">{{$product->tagline}}
+</p>
+@endif
 
 <!--this is for the price-->
 <p class="text-lg font-bold">
@@ -48,7 +54,7 @@ class="w-full h-80 object-cover rounded-xl">
 
 <!--this is for how much stock is left for each product-->
 <p class="text-sm">
-    @if ($product->stock_level !== null && $product->stock_level > 0)
+    @if ($product->stock_level>10)
     <span class="text-green-600 font-medium">In Stock:{{$product->stock_level}}</span>
     @else
     <span class="text-red-600 font-medium">Out of Stock</span>
@@ -62,31 +68,50 @@ class="w-full h-80 object-cover rounded-xl">
     <li>[icon] Feature 3</li>
 </ul>
 
-<!--this is for letting the customer know how much product they have selected-->
-<div class="flex items-center gap-3">
-    <span class="font-semibold">Quantity:</span>
-    <select class="select select-bordered select-sm w-20 rounded">
-        @for ($i=1; $i <=10; $i++)
-        <option>{{$i}}</option>
-        @endfor
-</select>
-</div>
 
 <!--this is is for the basket (button)-->
-<button class="btn btn-neutral w-full md:w-64 rounded-full text-white mt-12">
-Add To Basket
-</button>
+<form method="post" action="{{URL::to('/basket/add')}}">
+    <input type="hidden" value="{{$product->id}}" name="product_id">
+    <!--this is for letting the customer know how much product they have selected-->
+    <span class="font-semibold">Quantity:</span>
+    <select class="select select-bordered select-sm w-20 rounded" name="amount">
+        @for ($i=1; $i <=10; $i++)
+            <option>{{$i}}</option>
+        @endfor
+    </select>
+    <input class="btn btn-neutral w-full md:w-64 rounded-full text-white mt-42" type="submit" value="Add to basket">
+    @csrf
+
+</form>
+    @if (session('error'))
+        <p>{{session('error')}}</p>
+    @endif
 </div>
 </div>
 
 <!--this is for the information on each product-->
-@if($product->description)
+@if($product->desription)
 <div class="border-t border-neutral/50 mt-14 pt-10">
     <h2 class="text-center text-xl font-bold mb-4">
         Product Description
 </h2>
 <p class="text-sm md:text-base leading-relaxed">
     {{$product->description}}
+</p>
+</div>
+@endif
+
+<!--this is for product information-->
+@if(isset($product->specification))
+<div class="border-t border-neutral/50 mt-14 pt-10">
+    <h2 class="text-center text-xl font-bold mb-4">
+        Product specification
+</h2>
+<p class="text-center text-xl font-bold mb-4">
+    Product specification
+</h2>
+<p class="text-sm md:text-base leading-relaxed">
+    {{$product->specification}}
 </p>
 </div>
 @endif
