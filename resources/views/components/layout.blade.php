aa0579acb8a098225019ed13751880d4035c1468
Saihaj Johal <personal@sjohal.net>
2025-12-12 14:39:57 +0000

    Added menu for log in and log out

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,8 +20,16 @@
         </div>
         <div class="navbar-end">
             <a class="btn btn-ghost" href="{{URL::to('/basket')}}"><img src="{{ URL::to('/') }}/pageicons/cart.svg" alt="Picture of a shopping cart" class="size-10"></a>
-            <a class="btn btn-ghost" href="{{URL::to('/register')}}"><img src="{{ URL::to('/') }}/pageicons/profile-picture.svg" alt="Picture for account" class="size-10"></a>
-            @auth<a class="btn btn-ghost" href="{{URL::to('/logout')}}">Log out</a>@endauth
+            <div class="dropdown dropdown-end">
+                <div class="btn btn-ghost" role="button" tabindex="0">
+                    <img src="{{ URL::to('/') }}/pageicons/profile-picture.svg" alt="Picture for account" class="size-10">
+                </div>
+                <ul class="dropdown-content menu menu-md rounded-box bg-base-200 p-2 mt-3 text-base-content shadow" tabindex="-1">
+                    @guest<a class="btn btn-ghost" href="{{URL::to('/login')}}">Log in</a>@endguest
+                    @guest<a class="btn btn-ghost" href="{{URL::to('/register')}}">Register</a>@endguest
+                    @auth<a class="btn btn-ghost" href="{{URL::to('/logout')}}">Log out</a>@endauth
+                </ul>
+            </div>
         </div>
     </nav>
     <main class="">
