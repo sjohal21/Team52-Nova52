57722d12868fef2fda7846e8c55cf6a236f15806
Saihaj Johal <personal@sjohal.net>
2026-01-22 14:41:13 +0000

    Improved basket page through having products in the basket form a column

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -8,37 +8,39 @@
                     <div class="card-body">
                         <h1 class="text-2xl font-bold text-center">Your Order</h1>
                         <div class="divider"></div>
-                        <div class="flex justify-between mb-10 font-bold text-lg">
+                        <div class="flex flex-col justify-between mb-10 font-bold text-lg">
                             @foreach($items as $item)
                                 <span>{{$item->product->name}}</span>
-                                <span>Quantity: {{$item->amount}}</span>
                             <div class="card">
                                 <div class="card-body">
-                                    <form method="post" action="{{URL::to("/basket/remove")}}">
-                                        <input type="hidden" name="product_id" value="{{$item->product->id}}">
-                                        <input type="submit" value="Remove Item" class="btn">
-                                        @csrf
-                                    </form>
-                                    <form method="post" action="{{URL::to("/basket/update")}}">
-                                        <input type="number" name="amount" value="{{$item->amount}}">
-                                        <input type="hidden" name="product_id" value="{{$item->product->id}}">
-                                        <input type="submit" value="Update quantity" class="btn">
-                                        @csrf
-                                    </form>
+                                    <div class="flex flex-col items-end">
+                                        <form method="post" action="{{URL::to("/basket/update")}}">
+                                            <input type="number" name="amount" value="{{$item->amount}}" class="w-12">
+                                            <input type="hidden" name="product_id" value="{{$item->product->id}}">
+                                            <input type="submit" value="Update quantity" class="btn m-2">
+                                            @csrf
+                                        </form>
+                                        <form method="post" action="{{URL::to("/basket/remove")}}">
+                                            <input type="hidden" name="product_id" value="{{$item->product->id}}">
+                                            <input type="submit" value="Remove Item" class="btn m-2">
+                                            @csrf
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
-
                             @endforeach
+                        </div>
+                        <div class="divider"></div>
+                        <div class="card font-bold text-lg">
+                            <span>Items in basket: {{$count}}</span>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
             <div class="flex-1 bg-base-300 p-6 rounded-xl shadow">
                 <div class="card">
                     <div class="card-body">
-                        <h1 class="text-2xl font-bold text-center">Basket Summary</h1>
+                        <h1 class="text-2xl font-bold text-center">Basket summary</h1>
                         <div class="divider"></div>
                         <div class="flex justify-between mb-10 font-bold text-lg">
                             <span>Total:</span>
@@ -46,13 +48,13 @@
                         </div>
                         <a class="btn" href="{{URL::to("/checkout")}}">Check out</a>
                         <!-- If nothing being added to basket then basket has not been created -->
-                         @if(session('error'))
-                             <p class="alert alert-error p-4 mb-4">
-                                 {{ session('error') }}
+                        @if(session('error'))
+                            <p class="alert alert-error p-4 mb-4">
+                                {{ session('error') }}
                             </p>
                         @endif
                     </div>
-                    </div>
                 </div>
+            </div>
         </div>
 </x-layout>
