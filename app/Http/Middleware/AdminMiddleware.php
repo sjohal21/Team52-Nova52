c91ef4cfc83a52d97bf57d638577d00fa1e13980
Saihaj Johal <personal@sjohal.net>
2026-01-27 13:08:52 +0000

    Fixed capitalisation of admin middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
@@ -16,7 +17,7 @@ class AdminMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         //checks if the user is authorised and if their role is admin
-        if (!Auth::check() || Auth::user()->role !== 'admin') {
+        if (!Auth::check() || Auth::user()->role !== 'Admin') {
         return redirect('/')->withError('error','You are not authorised to access this page');
         }
         return $next($request);
