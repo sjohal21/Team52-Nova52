929c0da1635f4f5b38954f4824f5656d9afa4033
Saihaj Johal <personal@sjohal.net>
2026-01-27 13:29:54 +0000

    Added basic admin dashboard layout

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-adminlayout>
+    <p>Administration page coming soon</p>
+</x-adminlayout>
--- resources/views/components/adminlayout.blade.php
+++ resources/views/components/adminlayout.blade.php
@@ -6,21 +6,36 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <nav class="navbar bg-black text-white">
-        <div class="navbar-start">
-            <a class="btn btn-ghost text-xl">Nova52 Admin</a>
-        </div>
-        <div class="navbar-end">
-            <div class="dropdown dropdown-end">
-                <div class="btn btn-ghost" role="button" tabindex="0">
-                    <img src="{{ URL::to('/') }}/pageicons/profile-picture.svg" alt="Picture for account" class="size-10">
+    <div class="drawer">
+        <input id="leftsidebar" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+            <nav class="navbar bg-black text-white w-full">
+                <div class="navbar-start">
+                    <label for="leftsidebar" class="btn drawer-button btn-ghost">|||</label>
+                    <a class="btn btn-ghost text-xl">Nova52 Admin</a>
                 </div>
-                <ul class="dropdown-content menu menu-md rounded-box bg-base-200 p-2 mt-3 text-base-content shadow" tabindex="-1">
-                    @guest<a class="btn btn-ghost" href="{{URL::to('/login')}}">Log in</a>@endguest
-                    @guest<a class="btn btn-ghost" href="{{URL::to('/register')}}">Register</a>@endguest
-                    @auth<a class="btn btn-ghost" href="{{URL::to('/logout')}}">Log out</a>@endauth
-                </ul>
-            </div>
+                <div class="navbar-end">
+                    <div class="dropdown dropdown-end">
+                        <div class="btn btn-ghost" role="button" tabindex="0">
+                            <img src="{{ URL::to('/') }}/pageicons/profile-picture.svg" alt="Picture for account" class="size-10">
+                        </div>
+                        <ul class="dropdown-content menu menu-md rounded-box bg-base-200 p-2 mt-3 text-base-content shadow" tabindex="-1">
+                            @guest<a class="btn btn-ghost" href="{{URL::to('/login')}}">Log in</a>@endguest
+                            @guest<a class="btn btn-ghost" href="{{URL::to('/register')}}">Register</a>@endguest
+                            @auth<a class="btn btn-ghost" href="{{URL::to('/logout')}}">Log out</a>@endauth
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <main>
+                {{$slot}}
+            </main>
         </div>
-    </nav>
+        <div class="drawer-side bg-black text-white flex">
+            <label for="leftsidebar" class="drawer-overlay btn btn-ghost justify-start">|||</label>
+            <ul class="menu p-4 w-80">
+                <li><p>Placeholder 1</p></li>
+            </ul>
+        </div>
+    </div>
 </body>
