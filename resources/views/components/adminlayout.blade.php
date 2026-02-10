cb7009e66877d23f466ff4cdf933b324f64e3bf0
Saihaj Johal <personal@sjohal.net>
2026-02-10 14:20:47 +0000

    Added basic admin layout

--- resources/views/components/adminlayout.blade.php
+++ resources/views/components/adminlayout.blade.php
@@ -6,21 +6,31 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <nav class="navbar bg-black text-white">
-        <div class="navbar-start">
-            <a class="btn btn-ghost text-xl">Nova52 Admin</a>
-        </div>
-        <div class="navbar-end">
-            <div class="dropdown dropdown-end">
-                <div class="btn btn-ghost" role="button" tabindex="0">
-                    <img src="{{ URL::to('/') }}/pageicons/profile-picture.svg" alt="Picture for account" class="size-10">
-                </div>
-                <ul class="dropdown-content menu menu-md rounded-box bg-base-200 p-2 mt-3 text-base-content shadow" tabindex="-1">
-                    @guest<a class="btn btn-ghost" href="{{URL::to('/login')}}">Log in</a>@endguest
-                    @guest<a class="btn btn-ghost" href="{{URL::to('/register')}}">Register</a>@endguest
-                    @auth<a class="btn btn-ghost" href="{{URL::to('/logout')}}">Log out</a>@endauth
-                </ul>
+<nav class="navbar bg-black text-white">
+    <div class="dropdown lg:hidden">
+        <div tabindex="0" role="button" class="btn bg-black text-white btn-ghost">|||</div>
+        <ul class="dropdown-content bg-black text-white rounded w-30" tabindex="-1">
+            <a class="btn btn-ghost" href="{{URL::to('/admin/dashboard')}}">Dashboard</a>
+        </ul>
+    </div>
+    <div class="navbar-start">
+        <a class="btn btn-ghost text-xl" href="/">Nova52 Admin</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+        <a class="btn btn-ghost" href="{{URL::to('/admin/dashboard')}}">Dashboard</a>
+    </div>
+    <div class="navbar-end">
+        <div class="dropdown dropdown-end">
+            <div class="btn btn-ghost" role="button" tabindex="0">
+                <img src="{{ URL::to('/') }}/pageicons/profile-picture.svg" alt="Picture for account" class="size-10">
             </div>
+            <ul class="dropdown-content menu menu-md rounded-box bg-base-200 p-2 mt-3 text-base-content shadow" tabindex="-1">
+                <a class="btn btn-ghost" href="{{URL::to('/logout')}}">Log out</a>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
+<main class="">
+    {{$slot}}
+</main>
 </body>
