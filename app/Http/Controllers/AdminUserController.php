6d0b7b0a2a56e4930b3fc973e9c71e59229217c0
Kieron <kcmpsci@gmail.com>
2026-02-16 15:21:16 +0000

    Completed promote feature on admin user controller

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
-    //
+    public function show() {
+        //Get all of the users from the users table and order them by name
+        $users = User::query()
+          ->orderBy('name')
+          ->get();
+        return view('admin.users',['users' => $users]);
+    }
+
+    public function promote(Request $request) {
+        //makes sure that a valid user id exists for the user
+        $request->validate([
+          'user_id' => ['required','integer','exists:users,id']
+        ]);
+
+        //if the user doesnt exist an error is thrown
+        $user = User::findOrFail($request->'user_id');
+
+        if (user->role !== 'Admin') {
+            user->role == 'Admin';
+            user->save;
+        }
+        return redirect('/admin/users')->with('success','User has been successfully promoted!');
+    }
 }
