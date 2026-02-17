6d2e384894b41e1d7c1ac03fba5b7258012975df
Saihaj Johal <personal@sjohal.net>
2026-02-17 10:40:13 +0000

    Added placeholder for product image

--- resources/views/WishList.blade.php
+++ resources/views/WishList.blade.php
@@ -4,10 +4,15 @@
     </x-slot:title>
     <div class="p-10">
         @foreach($items as $item)
-            <div class="card bg-base-200">
-                <div class="card-body">
-                    <div class="card-title">{{$item->product->name}}</div>
-                    <p>{{$item->product->description}}</p>
+            <div class="flex-row w-96">
+                <div class="card bg-base-300">
+                    <p>Image</p>
+                </div>
+                <div class="card bg-base-200">
+                    <div class="card-body">
+                        <div class="card-title">{{$item->product->name}}</div>
+                        <p>{{$item->product->description}}</p>
+                    </div>
                 </div>
             </div>
         @endforeach
