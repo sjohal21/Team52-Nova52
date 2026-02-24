cf8465487f94dae3a7852cacd5e7de3b9f6888a6
Saihaj Johal <personal@sjohal.net>
2026-02-24 14:56:19 +0000

    Added basic layout

---
+++ app/Http/Controllers/ManageDetailsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageDetailsController extends Controller
+{
+    public function index()
+    {
+        // TODO: show page to change user details
+    }
+
+    public function changeEmail(Request $request)
+    {
+        // TODO: add function to change the user's email
+    }
+
+    public function changePassword(Request $request)
+    {
+        // TODO: add function to change the user's password
+    }
+}
