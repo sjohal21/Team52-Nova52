2dba85f5f1537540f86d8c683bd982c45588c00b
Saihaj Johal <personal@sjohal.net>
2026-03-03 14:38:19 +0000

    Added password change in controller

--- app/Http/Controllers/ManageDetailsController.php
+++ app/Http/Controllers/ManageDetailsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ManageDetailsController extends Controller
 {
@@ -21,11 +22,20 @@ class ManageDetailsController extends Controller
         {
             $user = Auth::user();
             $user->email = $validatedEmail['email'];
+            $user->save();
+            redirect('user.managedetails')->with('success', 'Your email changed successfully');
         }
     }
 
     public function changePassword(Request $request)
     {
-        // TODO: add function to change the user's password
+        $validatedPassword = $request->validate(['password' => 'required|min:8']);
+        if($validatedPassword['password'] != null)
+        {
+            $user = Auth::user();
+            $user->password = Hash::make($validatedPassword['password']);
+            $user->save();
+            redirect('user.managedetails')->with('success', 'Your password successfully changed');
+        }
     }
 }
