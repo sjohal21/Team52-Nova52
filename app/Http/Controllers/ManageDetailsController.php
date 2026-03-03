e4e7f03fa306b117ac87c56bc1b9e756f1a03ee5
Saihaj Johal <personal@sjohal.net>
2026-03-03 14:38:57 +0000

    Removed TODO tags

--- app/Http/Controllers/ManageDetailsController.php
+++ app/Http/Controllers/ManageDetailsController.php
@@ -10,13 +10,11 @@ class ManageDetailsController extends Controller
 {
     public function index()
     {
-        // TODO: show page to change user details
         return view('user.managedetails');
     }
 
     public function changeEmail(Request $request)
     {
-        // TODO: add function to change the user's email
         $validatedEmail = $request->validate(['email' => 'required|email']);
         if($validatedEmail['email'] != null)
         {
