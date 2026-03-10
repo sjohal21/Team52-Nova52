645af079266ca534b1c38f431e53d593c7417c9b
u-rafiq <rafiquzayr18@gmail.com>
2026-03-10 15:10:06 +0000

    Added edit product page routes

---
+++ app/Http/Controllers/AdminProductController.php
@@ -0,0 +1,12 @@
+<?php 
+
+use Illuminate\Http\Request;
+
+class AdminProductController extends Controller {
+
+    // Admin Edit Product Page Route
+    public function editProductPage() {
+        return view('admin.editProduct');
+    }
+    
+}
