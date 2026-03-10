fc10cac02f08513c897b37a198d0b27057adaf63
Saihaj Johal <personal@sjohal.net>
2026-03-10 12:54:29 +0000

    Added add review functionality

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reviews;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
     public function index()
     {
         return view('reviews.addreview');
+    }
+
+    public function addReview(Request $request)
+    {
+        $validatedData = $request->validate(["product_id" => "required", "description" => "required");
+        if ($validatedData)
+        {
+            $userID = Auth::id();
+            Reviews::create([
+                'user_id' => $userID,
+                'product_id' => $request->product_id,
+                'description' => $request->description
+            ]);
+            return redirect('/products/' . $request->product_id)->with('success', 'Review added successfully');
+        }
     }
 }
