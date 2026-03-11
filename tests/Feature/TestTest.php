e443b8243ab342c52dec9dee8e71a6fcfb8719ac
Puzzle-Head94 <benjamin14brown@gmail.com>
2026-03-11 20:35:58 +0000

    Test for user to log in

--- tests/Feature/TestTest.php
+++ tests/Feature/TestTest.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use phpDocumentor\Reflection\Types\Null_;
+uses(RefreshDatabase::class);
+// Checkout, Add to basket, Change quantity
+
 // Checks to see that the main page is loading correctly
 
 it('main page loads correctly', function () {
@@ -11,3 +16,24 @@ it('main page loads correctly', function () {
 
 });
 
+it ('User can log in', function() {
+    $user = \App\Models\User::factory()->create([
+        'name' => 'Testing User',
+        'phone_number' => '0123456789',
+        'address' => null,
+        'email' => 'testing@email.com',
+        'role' => 'Customer',
+        'password' => bcrypt('test')
+    ]);
+
+    $response = $this -> post(route('login'), [
+        'email' => $user -> email,
+        'password' => 'test'
+    ]);
+
+    $response -> assertRedirect(route('home.index'));
+    $this -> assertAuthenticatedAs($user);
+});
+
+
+//TODO: Add Item to Basket
