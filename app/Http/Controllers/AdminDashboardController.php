c99134751dabf4a27bb56a629b7319558a6a5420
Kieron <kcmpsci@gmail.com>
2026-03-18 22:03:47 +0000

    Updated dashboard controller to connect with model

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -13,43 +13,35 @@ class AdminDashboardController extends Controller
 {
 
   public function show() {
-
-        $users = User::all();
       //value that we will use to decide whether an item is low on stock
         $lowStockBound = 6;
         //amount of entries for the recent activity
         $activityLimit = 6;
-
         //number of the amount of products that are currently low on stock
         $lowStockCount = Product::WhereBetween('stock_level',[1,$lowStockBound])->count();
-
         //number of the amount of orders that are currently pending
-        $processingOrdersCount = Order::query()
+        $pendingOrdersCount = Order::query()
         ->where('status','Pending')
         ->count();
-
         //number of the amount of products that are currently out of stock
         $outOfStockCount = Product::query()
         ->where('stock_level',0)
         ->count();
-
         //number of the amount of orders in progress
         $orderInProgressCount = Order::where('status','Processing')->count();
-    
         //gets the newest admin logs
-        $recentActivities = AdminLog::latest()
+        $recentActivities = AdminLog::with('user')
+        ->latest()
         ->take($activityLimit)
         ->get();
-
         //returning the view for the admin dashboard
         return view('admin.dashboard',[
         'lowStockBound' => $lowStockBound,
         'lowStockCount' => $lowStockCount,
-        'processingOrdersCount' => $processingOrdersCount,
+        'pendingOrdersCount' => $pendingOrdersCount,
         'outOfStockCount' => $outOfStockCount,
         'orderInProgressCount' => $orderInProgressCount,
         'recentActivities' => $recentActivities,
-        'users' => $users
         ]);
     }
 }
