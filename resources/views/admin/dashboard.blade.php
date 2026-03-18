ee5b9ef02b1f4cf53b37556243bde3e7ba5f9058
Saihaj <personal@sjohal.net>
2026-03-18 15:40:21 +0000

    Added links to product page on admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,7 +14,7 @@
                     <div class="card-body">
                         <h2 class="card-title font-bold justify-center">Low stock items</h2>
                         <p>{{$lowStockCount}} low stock items</p>
-                        <a class="btn btn-ghost bg-black text-white rounded-md">View stock</a>
+                        <a class="btn btn-ghost bg-black text-white rounded-md" href="{{URL::to('/admin/products')}}">View products</a>
                     </div>
                 </div>
                 <div class="card bg-base-200 m-5">
@@ -28,7 +28,7 @@
                     <div class="card-body">
                         <h2 class="card-title font-bold justify-center">Out of stock items</h2>
                         <p>{{$outOfStockCount}} items out of stock</p>
-                        <a class="btn btn-ghost bg-black text-white rounded-md">View stock</a>
+                        <a class="btn btn-ghost bg-black text-white rounded-md" href="{{URL::to('/admin/products')}}">View products</a>
                     </div>
                 </div>
             </div>
