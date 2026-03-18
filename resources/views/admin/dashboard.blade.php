d6e40b0c0d0fd619aa15c326e83ceadb1a027814
Kieron <kcmpsci@gmail.com>
2026-03-18 22:12:59 +0000

    removed semi colon

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
                 <div class="card bg-base-200 m-5">
                     <div class="card-body">
                         <h2 class="card-title font-bold justify-center">Pending Orders</h2>
-                        <p>{{$pendingOrdersCount}} Pending Orders:</p>
+                        <p>{{$pendingOrdersCount}} Pending Orders</p>
                         <a class="btn btn-ghost bg-black text-white rounded-md" href="{{URL::to('/admin/orders')}}">Manage orders</a>
                     </div>
                 </div>
