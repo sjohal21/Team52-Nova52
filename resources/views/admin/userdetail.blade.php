a90909cc15d526f6f1814835bb510878de89c21a
Saihaj <personal@sjohal.net>
2026-03-21 08:18:07 +0000

    Added basic user detail admin page

--- resources/views/admin/userdetail.blade.php
+++ resources/views/admin/userdetail.blade.php
@@ -3,5 +3,26 @@
     <x-slot:title>
         Manage account for {{$user->name}}
     </x-slot:title>
-    <p>Under construction</p>
+    <div class="p-5">
+        <div class="card bg-base-200 p-5 w-auto text-center">
+            <h2 class="text-xl font-bold">{{$user->name}}</h2>
+            <div class="card-body">
+                <p>ID: #{{$user->id}}</p>
+            </div>
+        </div>
+        <div class="flex flex-row p-5 justify-center">
+            <div class="card bg-base-200 p-5 m-5 min-w-md">
+                <h2 class="card-title">Customer details</h2>
+                <div class="card-body">
+                    <p>Name: {{$user->name}}</p>
+                </div>
+            </div>
+            <div class="card bg-base-200 p-5 m-5 min-w-md">
+                <h2 class="card-title">User information</h2>
+                <div class="card-body">
+                    <p>Role: {{$user->role}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-adminlayout>
