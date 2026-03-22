c1dda11563c005dc3693eaa9c50eaf5afd3a5e87
Kieron <kcmpsci@gmail.com>
2026-03-22 11:27:20 +0000

    changed admin product management controller name

--- app/Http/Controllers/AdminProductManagementController.php
+++ app/Http/Controllers/AdminProductManagementController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminProductController extends Controller
+class AdminProductManagementController extends Controller
 {
     //
 }
