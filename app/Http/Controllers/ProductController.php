1a19ac9adc40dc6f81c46d8bd0ede8c38be4b764
Mehvish Mirza <mehvish.mirzzaa@gmail.com>
2026-03-23 22:46:15 +0000

    Changed minor alignment issue for home page icons and issue with filter/search

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,50 +6,58 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\Reviews;
 use Illuminate\Http\Request;
-use function Pest\Laravel\get;
 
 class ProductController extends Controller
 {
-    public function index() {
-    //Gets all of the products from the database
+    public function index()
+    {
+        // Gets all of the products from the database
         $products = Product::all();
 
-        return view('products.index',compact('products'));
+        return view('products.index', compact('products'));
     }
 
-    public function search(Request $request) {
+    public function search(Request $request)
+    {
+        $query = Product::query();
 
-        //Make sure that the search isnt empty
-        $request->validate([
-        'search' => 'required|string|max:255'
-        ]);
-
-        //Retrieve the entered item for the search
-        $searchedItem = $request->input('search');
-
-        //Search for similar product to the product that is entered
-        $products = Product::where('name','like',"%{$searchedItem}%")->get();
-
-        //if there are no products that match show a message
-        if($products->isEmpty()) {
-            return redirect()->back()->with('message','No products that match your search found!!');
+        // Search by product name
+        if ($request->filled('search')) {
+            $query->where('name', 'like', '%' . $request->search . '%');
         }
 
-        return view('products.index',compact('products'));
+        // Filter by category
+        if ($request->filled('category')) {
+            $category = Category::where('name', $request->category)->first();
+
+            if ($category) {
+                $query->where('category_id', $category->id);
+            }
+        }
+
+        $products = $query->get();
+
+        return view('products.index', compact('products'));
     }
 
     public function searchByCategory(string $categoryName)
     {
-        $category = Category::where('name',$categoryName)->first();
-        $products = $category->products;
-        if ($products->isEmpty())
-        {
-            return redirect()->back()->with('message','No products that match your search found!');
+        $category = Category::where('name', $categoryName)->first();
+
+        if (!$category) {
+            return view('products.index', ['products' => collect()]);
         }
-        return view('products.index',compact('products'));
+
+        $products = $category->products;
+
+        return view('products.index', compact('products'));
     }
 
-    public function showOne(Product $product) {
-        return view('products.show',['product'=>$product, 'reviews'=>Reviews::where('product_id',($product->id))]);
+    public function showOne(Product $product)
+    {
+        return view('products.show', [
+            'product' => $product,
+            'reviews' => Reviews::where('product_id', $product->id)
+        ]);
     }
 }
